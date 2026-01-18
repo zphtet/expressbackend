fa2fc5b33c44eee07cd045e3ea32b75c1b31c888
zphtet <zinphtet.215108@gmail.com>
2026-01-18 14:38:39 +0630

    update cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express'
 import type { Request, Response } from 'express'
 import { prisma } from '../lib/prisma.js'
+import cors from 'cors'
 const app = express()
 const port = process.env.PORT || 3005
 
+
+app.use(cors())
 app.get('/', (_ : Request, res : Response) => {
   res.send('Hello World! this is the main route')
 })
